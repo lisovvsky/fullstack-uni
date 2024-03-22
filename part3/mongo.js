3a9c6e5084cef69072c67d787e4e82d2a505dc08
lisovvsky <klaudiuszkacz16@gmail.com>
2024-03-22 09:18:12 +0100

    Update mongo.js

--- part3/mongo.js
+++ part3/mongo.js
@@ -8,7 +8,7 @@ if ( process.argv.length < 3 ) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://ghoulgager:${password}@cluster0.bnpmn3c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  `mongodb+srv://klaudiuszkacz16:${password}@cluster0.2ectbxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.connect(url)
 
